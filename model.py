2e8d342c46c437c2fb88f91f835aca41ddf14cec
Ubuntu <timefly1989@126.com>
2020-10-25 18:01:38 +0800

    ubuntu

---
+++ model.py
@@ -0,0 +1,41 @@
+import torch as th
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Critic(nn.Module):
+    def __init__(self, n_agent, dim_observation, dim_action):
+        super(Critic, self).__init__()
+        self.n_agent = n_agent
+        self.dim_observation = dim_observation
+        self.dim_action = dim_action
+        obs_dim = dim_observation * n_agent
+        act_dim = self.dim_action * n_agent
+        self.FC1 = nn.Linear(obs_dim, 256)
+        self.FC2 = nn.Linear(256 + act_dim, 512)
+        self.FC3 = nn.Linear(512, 256)
+        self.FC4 = nn.Linear(256, 1)
+
+    # obs: batch_size * obs_dim
+    def forward(self, obs, acts):
+        result = F.relu(self.FC1(obs))
+        combined = th.cat([result, acts], 1)
+        result = F.relu(self.FC2(combined))
+        return self.FC4(F.relu(self.FC3(result)))
+
+
+class Actor(nn.Module):
+    def __init__(self, dim_observation, dim_action):
+        super(Actor, self).__init__()
+        self.FC1 = nn.Linear(dim_observation, 256)
+        self.FC2 = nn.Linear(256, 512)
+        self.FC3 = nn.Linear(512, 256)
+        self.FC4 = nn.Linear(256, dim_action)
+
+    # action output between -2 and 2
+    def forward(self, obs):
+        result = F.relu(self.FC1(obs))
+        result = F.relu(self.FC2(result))
+        result = F.relu(self.FC3(result))
+        result = th.tanh(self.FC4(result))
+        return result
